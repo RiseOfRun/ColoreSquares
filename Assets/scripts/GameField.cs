c9d58ee114c45d41a33c591c97299751b36499e8
Runi <runi54rus@gmail.com>
2021-09-08 00:54:09 +0700

    CoreGamePlayDone

--- Assets/scripts/GameField.cs
+++ Assets/scripts/GameField.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Transactions;
+using TreeEditor;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Diagnostics;
@@ -12,16 +14,17 @@ using Random = UnityEngine.Random;
 public class GameField : MonoBehaviour
 {
     public float Padding = 2;
-    public GameObject TilePrefab;
-    public Cell CellPrefab;
+    public Square SquarePrefab;
+    public GameObject FramePrefab;
     public bool CanSpawn = true;
-    
+    public bool CanMove = true;
     public int rows = 4;
     public int cols = 4;
     private float cellSize;
     private float space;
-    Cell[,] field;
-    private List<Cell> cells = new List<Cell>();
+    Square[,] field;
+    List<int[]> emptyCells= new List<int[]>();
+    
     
     // Start is called before the first frame update
     void Start()
@@ -33,115 +36,262 @@ public class GameField : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            SpawnRandomCell();
+            SpawnRandomSquare();
+            // SpawnRandomCell();
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            OnInput(Vector2.down);
+            OnInput(Vector2Int.down);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            OnInput(Vector2.left);
+            OnInput(Vector2Int.left);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            OnInput(Vector2.right);
+            OnInput(Vector2Int.right);
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            OnInput(Vector2.up);
+            OnInput(Vector2Int.up);
         }
     }
 
-    private void OnInput(Vector2 diraction)
+    private void OnInput(Vector2Int diraction)
     {
-        int start = diraction.x > 0 || diraction.y < 0 ? rows-1 : 0;
-        int dir = diraction.x > 0 || diraction.y < 0 ? -1 : 1;
-        
-        for (int i = start+dir; i >= 0 && i < rows; i+=dir)
+        // int start = diraction.x > 0 || diraction.y < 0 ? rows-1 : 0;
+        // int dir = diraction.x > 0 || diraction.y < 0 ? -1 : 1;
+        //
+        // for (int i = start+dir; i >= 0 && i < rows; i+=dir)
+        // {
+        //     for (int j = 0; j < rows; j++)
+        //     {
+        //         bool row = diraction.x == 0;
+        //         Cell currentCell = !row ? field[j,i] : field[i,j];
+        //         if (currentCell.empty)
+        //         {
+        //             continue;
+        //         }
+        //         
+        //         Cell nextCell = null;
+        //         for (int k = i-dir; k >=0 && k<rows; k-=dir)
+        //         {
+        //             row = diraction.x == 0;
+        //             Cell tmp = row ? field[k,j] : field[j,k];
+        //             if (tmp.empty)
+        //             {
+        //                 nextCell = tmp;
+        //                 continue;
+        //             }
+        //             if (!tmp.HasMerged)
+        //             {
+        //                 break;
+        //                 nextCell = tmp;
+        //             }
+        //         }
+        //         if (nextCell != null)
+        //         { 
+        //             MoveTo(currentCell,nextCell);
+        //         }
+        //     }
+        // }
+
+        //     bool isOpposite = ;
+        //
+        //     //for x in grid:
+        //     //  for y in grid:
+        //     //    MoveCell(coords, direction)
+        // }
+        //
+        //
+        //
+        // private Cell Get(Vector2Int pos){}
+        // private void Set(Vector2Int pos, Cell cell){}
+        bool moveDone = false;
+        foreach (var square in field)
         {
-            for (int j = 0; j < rows; j++)
+            if (square != null)
             {
-                bool row = diraction.x == 0;
-                Cell currentCell = !row ? field[j,i] : field[i,j];
-                if (currentCell.empty)
+                square.CanMerge = true;
+            }
+        }
+        
+        int start = diraction.x > 0 || diraction.y > 0 ? rows - 1 : 0;
+        int dir = diraction.x > 0 || diraction.y > 0 ? -1 : 1;
+        bool Opposite = diraction.x == 0;
+        
+        for (int i = start+dir; i>=0 && i<rows; i+=dir)
+        {
+            for (int j = 0; j<rows; j++)
+            {
+
+                Vector2Int xy = Opposite ? new Vector2Int(j, i) : new Vector2Int(i, j); 
+                Square current = Get(xy); 
+                if (current == null)
                 {
                     continue;
                 }
                 
-                Cell nextCell = null;
-                for (int k = i-dir; k >=0 && k<rows; k-=dir)
-                {
-                    row = diraction.x == 0;
-                    Cell tmp = row ? field[k,j] : field[j,k];
-                    if (tmp.empty)
-                    {
-                        nextCell = tmp;
-                        continue;
-                    }
-                    if (!tmp.HasMerged)
-                    {
-                        break;
-                        nextCell = tmp;
-                    }
-                }
-                if (nextCell != null)
-                { 
-                    MoveTo(currentCell,nextCell);
-                }
+                Move(xy,diraction,ref moveDone);
             }
+        }
+        if (moveDone)
+        {
+            SpawnRandomSquare();
+            CanMove = true;
+        }
+        else
+        {
+            CanMove = false;
         }
     }
 
-    void MoveTo(Cell from, Cell to)
+    private Square Get(Vector2Int pos)
     {
-        Debug.Log($"Moving object from {from.transform.position} to {to.transform.position}");
-        to.square = from.square;
-        to.square.gameObject.transform.position = to.transform.position;
-        from.empty = true;
-        to.empty = false;
+        return field[pos.x, pos.y];
     }
-    
+
+    private void Set(Vector2Int pos, Square square)
+    {
+        field[pos.x, pos.y] = square;
+    }
+    void MoveTo(Cell from, Cell to)
+        {
+            Debug.Log($"Moving object from {from.transform.position} to {to.transform.position}");
+            to.square = from.square;
+            to.square.gameObject.transform.position = to.transform.position;
+            from.empty = true;
+            to.empty = false;
+        }
+
+    void Move(Vector2Int currentPosition, Vector2Int diraction, ref bool someMoved)
+    {
+        Print();
+        Square current = Get(currentPosition);
+        Square next;
+        Vector2Int lastPosition = currentPosition;
+        Vector2Int nextPosition = currentPosition + diraction;
+
+        while (nextPosition.x < rows && nextPosition.x>=0 && nextPosition.y < cols && nextPosition.y >=0)
+        {
+            if (Get(nextPosition)==null)
+            {
+                currentPosition = nextPosition;
+                someMoved = true;
+            }
+            else
+            {
+                var nextSquare = Get(nextPosition);
+                if (nextSquare.Weight==current.Weight)
+                {
+                    if (nextSquare.CanMerge)
+                    {
+                        current.CanMerge = false;
+                        current.MergeAfterReachingTarget = nextSquare;
+                        currentPosition = nextPosition;
+                        someMoved = true;
+                    }
+                }
+
+                break;
+            }
+            nextPosition += diraction;
+        }
+
+        current.TargetPosition = currentPosition;
+        Set(lastPosition, null);
+        Set(currentPosition, current);
+    }
+
+    void Print()
+    {
+        string a = "";
+        for (int i = 0; i < field.GetLength(0); i++)
+        {
+            for (int j = 0; j < field.GetLength(1); j++)
+            {
+                a += $"({i},{j}) ";
+                    a += field[i, j] == null ? "-" : field[i, j].Weight.ToString();
+            }
+
+            a += '\n';
+        }
+        Debug.Log(a);
+    }
+
+    void Merge(Square from, Square to)
+    {
+        
+    }
 
     void BuildGrid()
     {
-        cellSize = CellPrefab.gameObject.GetComponentInChildren<SpriteRenderer>().bounds.size.x;
-        float fieldSize = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
-        space = (fieldSize - Padding * 2 - cellSize * cols) / (cols - 1);
-        Padding += cellSize / 2f;
-        field = new Cell[rows, cols];
+        field = new Square[rows,cols];
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
             {
-                Vector3 position = new Vector3(
-                    Padding + space * (j) + cellSize * j,
-                    -Padding - space * (i) - cellSize * i,
-                    0
-                );
-                field[i, j] = Instantiate(CellPrefab, transform);
-                field[i, j].transform.localPosition = position;
-                cells.Add(field[i, j]);
+                var Frame = Instantiate(FramePrefab, transform);
+                Frame.transform.localPosition=new Vector2(i,j);
+                field[i, j] = null;
             }
         }
+        CanSpawn = true;
+        SpawnRandomSquare();
+        SpawnRandomSquare();
     }
 
-    void SpawnRandomCell()
+    private void SpawnRandomSquare()
     {
-        if (!CanSpawn)
+        List<Vector2Int> pool = new List<Vector2Int>();
+        for (int i = 0; i < rows; i++)
         {
-            return;
+            for (int j = 0; j < cols; j++)
+            {
+                Vector2Int position = new Vector2Int(i,j);
+                if (Get(position)==null)
+                {
+                    pool.Add(position);
+                }
+            }
         }
-        System.Random r = new System.Random();
-        IEnumerable<Cell> emptyCells = cells.Where(x => x.empty);
-        int count = emptyCells.Count();
+        int count = pool.Count;
         if (count==0)
         {
             CanSpawn = false;
             return;
         }
-        Cell randomCell = emptyCells.ElementAt(r.Next(emptyCells.Count()-1));
-        randomCell.Spawn();
+        System.Random r = new System.Random();
+        Vector2Int randomCell = pool[r.Next(count - 1)];
+        SpawnSquare(randomCell);
+        
     }
+
+    public void SpawnSquare(Vector2Int position)
+    {
+        System.Random r = new System.Random();
+        Square square = Instantiate(SquarePrefab, transform);
+        int number = r.Next(100) > 95 ? 2 : 1;
+        square.Weight = number;
+        square.transform.localPosition = new Vector2(position.x,position.y);
+        square.TargetPosition = position;
+        Set(position,square);    
+    }
+    // void SpawnRandomCell()
+        // {
+        //     if (!CanSpawn)
+        //     {
+        //         return;
+        //     }
+        //     System.Random r = new System.Random();
+        //     IEnumerable<Cell> emptyCells = cells.Where(x => x.empty);
+        //     int count = emptyCells.Count();
+        //     if (count==0)
+        //     {
+        //         CanSpawn = false;
+        //         return;
+        //     }
+        //     Cell randomCell = emptyCells.ElementAt(r.Next(emptyCells.Count()-1));
+        //     randomCell.Spawn();
 }
--- Assets/scripts/Square.cs
+++ Assets/scripts/Square.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using TMPro.Examples;
 using UnityEngine;
@@ -7,7 +8,8 @@ using UnityEngine;
 public class Square : MonoBehaviour
 {
     public TextMeshPro text;
-
+    public Color[] colors;
+    public bool CanMerge = true;
     public int Weight
     {
         get => weight;
@@ -15,27 +17,57 @@ public class Square : MonoBehaviour
         {
             weight = value;
             text.text = weight > 0 ? Mathf.Pow(2, weight).ToString() : "";
+            
         }
     }
 
+    public float speed = 5;
     private int weight;
-    
-    
-    
+    public Vector2Int TargetPosition { get; set; }
+    public Square MergeAfterReachingTarget { get; set; }
 
-    public void Merge()
+    public static Square Merge(Square from, Square to)
     {
-        
+        from.Weight++;
+        Destroy(to.gameObject);
+        from.CanMerge = false;
+        from.SetColor();
+        return from;
+    }
+
+    void SetColor()
+    {
+        var sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.color = weight>colors.Length-1 ? colors.Last() : colors[weight];
     }
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
+        SetColor();
+        for (float i = 0; i < 1; i+=Time.deltaTime/0.3f)
+        {
+            float fade = Mathf.Lerp(0, 1, i);
+            fade = 1 - (1 - fade) * (1 - fade) * (1 - fade);
+            transform.localScale = new Vector3(fade,fade,fade);
+            yield return null;
+        }
         
+        transform.localScale = Vector3.one;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 nextPosition = Vector2.MoveTowards(transform.localPosition, TargetPosition, Time.deltaTime * speed);
+        transform.localPosition = nextPosition;
+
+        if ((Vector2) transform.localPosition == (Vector2) TargetPosition)
+        {
+            if (MergeAfterReachingTarget!=null)
+            {
+                Merge(this, MergeAfterReachingTarget);
+                MergeAfterReachingTarget = null;
+            }
+        }
     }
 }
